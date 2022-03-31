4c460d1566a35af1c664653ca9de38fddf8d714c
xhguleixin123 <18017158729@163.com>
2022-03-31 22:14:23 +0800

    ✨ feat: 配好start

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ function component()
     const element = document.createElement('div');
 
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-
+    
     return element;
 }
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,22 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
-  entry: {
-    index: './src/index.js',
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      title: 'Output Management',
-    }),
-  ],
-  output: {
-    filename: '[name].bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-    clean: true,
-  },
+    mode: 'development',
+    entry: {
+        index: './src/index.js',
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Output Management',
+        }),
+    ],
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true,
+    },
+    devtool: 'inline-source-map',
+    devServer: {
+        static: './dist',
+    },
 };
