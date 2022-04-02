39b9e176f9b57bef74e574cad2f9377b56a86bbd
xhguleixin123 <18017158729@163.com>
2022-04-02 14:40:14 +0800

    ✨ feat: 展示历史步骤记录

--- src/component/Game.js
+++ src/component/Game.js
@@ -37,6 +37,16 @@ class Game extends Component {
         const history = this.state.history;
         const current = history[history.length - 1];
         const winner = calculateWinner(current.squares);
+
+        const moves = history.map((step, move) => {
+            const desc = move ? 'Go to move #' + move : 'Go to game start';
+            return (
+                <li>
+                    <button>{desc}</button>
+                </li>
+            )
+        });
+
         let status;
         if (winner) {
             status = 'Winner: ' + winner;
@@ -49,12 +59,12 @@ class Game extends Component {
                 <div className="game-board">
                     <Board
                         squares={current.squares}
-                        onClick={(i)=>this.handleClick(i)}
+                        onClick={(i) => this.handleClick(i)}
                     />
                 </div>
                 <div className="game-info">
                     <div>{status}</div>
-                    <ol>{/* TODO */}</ol>
+                    <ol>{moves}</ol>
                 </div>
             </div>
         );
