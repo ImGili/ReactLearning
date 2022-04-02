e56f721f4bdbc1f54536a10ea7e4e303d8400afc
xhguleixin123 <18017158729@163.com>
2022-04-02 12:53:29 +0800

    ✨ feat: 轮流落子

--- src/component/Board.js
+++ src/component/Board.js
@@ -7,24 +7,30 @@ class Board extends Component {
 
         this.state = {
             squares: Array(9).fill(null),
+            xIsNext: true,
         };
     }
 
-    handleClick(i){
+    handleClick(i) {
         const squares = this.state.squares.slice();
-        squares[i] = 'X';
-        this.setState({squares:squares});
+        squares[i] = this.state.xIsNext ? 'X' : 'O';
+        this.setState(
+            {
+                squares: squares,
+                xIsNext: !this.state.xIsNext,                
+            }
+        );
     }
 
     renderSquare(i) {
         return <Square
             value={this.state.squares[i]}
-            onClick = {()=>this.handleClick(i)}
+            onClick={() => this.handleClick(i)}
         />;
     }
 
     render() {
-        const status = 'Next player: X';
+        const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
         return (
             <div>
                 <div className="status">{status}</div>
